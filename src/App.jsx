c961b4154b32dde19da5e0d615f66929a6d41c97
mongchongi <yeonguk0517@gmail.com>
2025-06-02 16:18:45 +0900

    최대 너비 세팅 및 가운데 정렬

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import styles from './App.module.css';
 import { Route, Routes } from 'react-router';
 import AppLayout from './layout/AppLayout/AppLayout';
 import HomePage from './pages/HomePage/HomePage';
@@ -6,16 +7,18 @@ import MovieDetailPage from './pages/MovieDetailPage/MovieDetailPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 const App = () => (
-  <Routes>
-    <Route path='/' element={<AppLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/movies'>
-        <Route index element={<MoviesPage />} />
-        <Route path=':id' element={<MovieDetailPage />} />
+  <div className={styles['container']}>
+    <Routes>
+      <Route path='/' element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/movies'>
+          <Route index element={<MoviesPage />} />
+          <Route path=':id' element={<MovieDetailPage />} />
+        </Route>
       </Route>
-    </Route>
-    <Route path='*' element={<NotFoundPage />} />
-  </Routes>
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+  </div>
 );
 
 export default App;

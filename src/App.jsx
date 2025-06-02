a445d6a849ec8d6df6092cdcce24bd193c81a97f
mongchongi <yeonguk0517@gmail.com>
2025-06-02 16:16:55 +0900

    페이지 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { Route, Routes } from 'react-router';
+import AppLayout from './layout/AppLayout/AppLayout';
+import HomePage from './pages/HomePage/HomePage';
+import MoviesPage from './pages/MoviesPage/MoviesPage';
+import MovieDetailPage from './pages/MovieDetailPage/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+const App = () => (
+  <Routes>
+    <Route path='/' element={<AppLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/movies'>
+        <Route index element={<MoviesPage />} />
+        <Route path=':id' element={<MovieDetailPage />} />
+      </Route>
+    </Route>
+    <Route path='*' element={<NotFoundPage />} />
+  </Routes>
+);
 
 export default App;

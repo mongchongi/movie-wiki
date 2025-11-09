f1514dd4b7ae83cb1760c1aacf5bec0a9ac516f5
Yeonguk Bak <yeonguk0517@gmail.com>
2025-11-09 18:36:28 +0900

    영화 정보 8 ~ 9 (2025. 11. 09. 과제)

--- src/pages/Homepage/components/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './MovieCard.module.css';
 import { faStarHalfStroke, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router';
 
 const MovieCard = ({ movie }) => {
   return (
@@ -8,29 +9,31 @@ const MovieCard = ({ movie }) => {
       className={styles['movie-card']}
       style={{ backgroundImage: `url(https://media.themoviedb.org/t/p/w600_and_h900_bestv2/${movie.poster_path})` }}
     >
-      <div className={styles['movie-card__info']}>
-        <span className={`${styles['movie-card__adult']} ${movie.adult ? '' : styles['movie-card__adult--all']}`}>
-          {movie.adult ? '+19' : 'ALL'}
-        </span>
-        <div className={styles['movie-card__genre-list']}>
-          {movie.genre_ids.map((genreId) => (
-            <div className={styles['movie-card__genre-item']} key={genreId}>
-              {genreId}
-            </div>
-          ))}
+      <Link to={`/movies/${movie.id}`}>
+        <div className={styles['movie-card__info']}>
+          <span className={`${styles['movie-card__adult']} ${movie.adult ? '' : styles['movie-card__adult--all']}`}>
+            {movie.adult ? '+19' : 'ALL'}
+          </span>
+          <div className={styles['movie-card__genre-list']}>
+            {movie.genre_ids.map((genreId) => (
+              <div className={styles['movie-card__genre-item']} key={genreId}>
+                {genreId}
+              </div>
+            ))}
+          </div>
+          <h3 className={styles['movie-card__title']}>{movie.title}</h3>
+          <div className={styles['movie-card__stats']}>
+            <p className={styles['movie-card__vote_average']}>
+              <FontAwesomeIcon icon={faStarHalfStroke} color='rgba(255, 193, 7, 1)' />
+              <span>{movie.vote_average.toFixed(1)}</span>
+            </p>
+            <p className={styles['movie-card__popularity']}>
+              <FontAwesomeIcon icon={faUsers} color='rgba(64, 224, 208, 1)' />
+              <span>{movie.popularity}</span>
+            </p>
+          </div>
         </div>
-        <h3 className={styles['movie-card__title']}>{movie.title}</h3>
-        <div className={styles['movie-card__stats']}>
-          <p className={styles['movie-card__vote_average']}>
-            <FontAwesomeIcon icon={faStarHalfStroke} color='rgba(255, 193, 7, 1)' />
-            <span>{movie.vote_average.toFixed(1)}</span>
-          </p>
-          <p className={styles['movie-card__popularity']}>
-            <FontAwesomeIcon icon={faUsers} color='rgba(64, 224, 208, 1)' />
-            <span>{movie.popularity}</span>
-          </p>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };

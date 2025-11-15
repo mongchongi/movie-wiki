d1e3017e767743e6c066968cf4bc403b42607a6b
Yeonguk Bak <yeonguk0517@gmail.com>
2025-11-15 13:22:16 +0900

    영화 정보 최종 (2025. 11. 14. 과제)

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,18 @@
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './NotFoundPage.module.css';
+import { Link } from 'react-router';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={styles['not-found']}>
+      <FontAwesomeIcon className={styles['not-found__icon']} icon={faTriangleExclamation} />
+      <p className={styles['not-found__message']}>Sorry, the page you requested could not be found.</p>
+      <Link className={styles['not-found__go-to-home']} to={'/'}>
+        Go to the homepage
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
